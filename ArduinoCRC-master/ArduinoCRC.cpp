afbfd863456ee8401c99cc8776cf2d2b77a8fc84
Masyukov Pavel <p.masyukov@gmail.com>
2022-12-14 21:59:08 +0300

    Update ArduinoCRC.cpp

--- ArduinoCRC-master/ArduinoCRC.cpp
+++ ArduinoCRC-master/ArduinoCRC.cpp
@@ -1,50 +1,45 @@
 #include <ArduinoCRC.h>
 
 
-//namespace TLV_Protocol
-//{
+/**
+ * @brief 
+ * 
+ */
+void Crc::start() 
+{
+	crc = 0xFF;
+}
 
-	/**
-	 * @brief 
-	 * 
-	 */
-	void Crc::start() 
+/**
+ * @brief 
+ * 
+ * @param b 
+ */
+void Crc::add(uint8_t b) 
+{
+	crc += b;
+}
+
+/**
+ * @brief 
+ * 
+ * @param data 
+ * @param dataLen 
+ */
+void Crc::add(uint8_t data[], uint16_t dataLen) 
+{
+	for(uint16_t i = 0; i < dataLen; i++) 
 	{
-		crc = 0xFF;
+		add(data[i]);
 	}
+}
 
-	/**
-	 * @brief 
-	 * 
-	 * @param b 
-	 */
-	void Crc::add(uint8_t b) 
-	{
-		crc += b;
-	}
-
-	/**
-	 * @brief 
-	 * 
-	 * @param data 
-	 * @param dataLen 
-	 */
-	void Crc::add(uint8_t data[], uint16_t dataLen) 
-	{
-		for(uint16_t i = 0; i < dataLen; i++) 
-		{
-			add(data[i]);
-		}
-	}
-
-	/**
-	 * @brief 
-	 * 
-	 * @return uint8_t 
-	 */
-	uint8_t Crc::get() 
-	{
-		return crc;
-	}
-
-//}
+/**
+ * @brief 
+ * 
+ * @return uint8_t 
+ */
+uint8_t Crc::get() 
+{
+	return crc;
+}
